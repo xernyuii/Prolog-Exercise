b1807e2ccb96032ba454ad2d32a4cd9d179cdd39
xernyuii <hfutxc.xern@gmail.com>
2017-10-30 23:59:52 +0800

    ex1

---
+++ mate.pl
@@ -0,0 +1,12 @@
+male(di).
+male(jianbo).
+female(xin).
+female(yuan).
+father(jianbo,di).
+father(fi,yuqing).
+father(di,yuqing).
+mother(xin,di).
+mother(yuan,yuqing).
+grandfather(X,Y):-father(X,Z),father(Z,Y).
+grandmother(X,Y):-mother(X,Z),father(Z,Y).
+daughter(X,Y):-father(X,Y),female(Y).
